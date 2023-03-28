243ca131f54283fd040f60e968d915144bfe3b2a
pedro <leoscotti03@gmail.com>
2023-03-28 11:39:51 -0300

    a

--- src/filas/Execute.java
+++ src/filas/Execute.java
@@ -13,35 +13,33 @@ public class Execute {
         System.out.println("(1) Adicionar | (2) Remover | (0) Encerrar");
         opcao = S.nextInt();
 
-    do {
-        switch (opcao) {
-            case 0:
-                if (!alunos.isEmpty())
-                    System.out.println("Ainda ha alunos a serem atendidos");
-                break;
-            case 1:
-                if (alunos.isFull())
-                    System.out.println("Fila cheia.");
-                else {
-                    System.out.println("Insira o RM para entrar na fila:");
-                    alunos.enqueue(S.nextInt());
-                }
-                break;
-            case 2:
-                if (alunos.isEmpty())
-                    System.out.println("Fila vazia.");
-                else {
-                    aux = alunos.dequeue();
-                    System.out.println("Aluno retirado: RM" + aux);
-                }
-                break;
-            default:
-                System.out.println("Opcao invalida");
-        }
-        System.out.println("(1) Adicionar | (2) Remover | (0) Encerrar");
-        opcao = S.nextInt();
-    } while (opcao != 0 && alunos.isEmpty());
-
-
+        do {
+            switch (opcao) {
+                case 0:
+                    if (!alunos.isEmpty())
+                        System.out.println("Ainda ha alunos a serem atendidos");
+                    break;
+                case 1:
+                    if (alunos.isFull())
+                        System.out.println("Fila cheia.");
+                    else {
+                        System.out.println("Insira o RM para entrar na fila:");
+                        alunos.enqueue(S.nextInt());
+                    }
+                    break;
+                case 2:
+                    if (alunos.isEmpty())
+                        System.out.println("Fila vazia.");
+                    else {
+                        aux = alunos.dequeue();
+                        System.out.println("Aluno retirado: RM" + aux);
+                    }
+                    break;
+                default:
+                    System.out.println("Opcao invalida");
+            }
+            System.out.println("(1) Adicionar | (2) Remover | (0) Encerrar");
+            opcao = S.nextInt();
+        } while (opcao != 0 && alunos.isEmpty());
     }
 }
